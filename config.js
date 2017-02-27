ef8012880620d1dc100b4f106bb496e08fec55e5
Max Weng <max.m.weng@gmail.com>
2017-02-28 00:26:19 +0800

    Updated to use the local dev server.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = '59431301.qcloud.la';
+var host = 'localhost:8000/wechat';
 
 var config = {
 
@@ -12,13 +12,13 @@ var config = {
         host,
 
         // 登录地址，用于建立会话
-        loginUrl: `https://${host}/login`,
+        loginUrl: `http://${host}/wxliteapp_login`,
 
         // 测试的请求地址，用于测试会话
-        requestUrl: `https://${host}/user`,
+        requestUrl: `http://${host}/wxliteapp_user`,
 
         // 测试的信道服务地址
-        tunnelUrl: `https://${host}/tunnel`,
+        tunnelUrl: `http://${host}/tunnel`,
     }
 };
 
